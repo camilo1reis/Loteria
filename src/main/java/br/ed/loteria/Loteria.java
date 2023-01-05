5eb7c7a836729ec2929acd6fea4ccbe77ecf7569
camilo1reis <camiloreis.jf@gmail.com>
2023-01-04 23:13:18 -0300

    Iniciando o projeto

---
+++ src/main/java/br/ed/loteria/Loteria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.ed.loteria;
+
+/**
+ *
+ * @author camilo
+ */
+public class Loteria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
